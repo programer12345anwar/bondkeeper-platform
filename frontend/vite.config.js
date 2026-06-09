d9968f4285c11d4cd51c32aff3f32b248b4cb695
Md Anwar Alam <mdanwar40212@gmail.com>
2026-06-10 01:24:42 +0530

    Cors config issue resolved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
+//    proxy: {
+//      '/api': {
+//        target: 'http://localhost:8080',
+//        changeOrigin: true,
+//      },
+//    },
   },
 });
